26bc01dabe4c2dc79739ce4d5ecdb8d8038cc296
Geraldo Furtado Jr <geraldofurtadojr@gmail.com>
2026-09-08 11:31:44 -0300

    Corrige sincronizacao entre aparelhos

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/convites-festa/sw.js', { scope: '/convites-festa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
