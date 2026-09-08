b75adaaaff6cf0c4ef0481f38941c1aa3a70fa74
Geraldo Furtado Jr <geraldofurtadojr@gmail.com>
2026-09-08 11:44:36 -0300

    Corrige caminhos dos arquivos no GitHub Pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/convites-festa/sw.js', { scope: '/convites-festa/' })})}
